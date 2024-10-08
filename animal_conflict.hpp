f3610f6f2dc2b5bde76fe694f5ccfbeaf3625363
bellou <rand@random.it>
2024-10-08 13:13:42 +0200

    added some basic comments

--- animal_conflict.hpp
+++ animal_conflict.hpp
@@ -1,48 +1,210 @@
 #pragma once 
 #include<iostream>
+#include<fstream>
 #include<random>
-#include<string>
+#include<algorithm>
+/**
+ * Animal class.
+ */
 class Animal{
     public:
-    size_t payoff; 
-    char previous_strategy;
-    char current_strategy;
-    char next_strategy;
-    size_t time_tolerance;
-    bool seriously_injured=false;
-    bool win=false;
+    /**
+     * Stores the payoff of each agent 
+     */
+    int payoff;
+    /*
+    * Store the previous move of agent, moves can be:
+        C: Conventional tactic.
+        D: Dangerous tactic. 
+        R: Retreat. 
+    */
+    char prev_move;
+    /**
+     * Store current move 
+     */
+    char curr_move;
+    /**
+     * Store next move 
+     */
+    char next_move;
+    /**
+     * Store if the agent won 
+     */
+    bool win;
+    /**
+     * Is the gent serious injured 
+     */
+    bool seriously_injured;
+    /**
+     * Store probability of injury 
+     */
+    double prob_injury;
+    /**
+     * Store the probability of retiliation needed in Retaliator and Probe-Retaliator 
+     * agents
+     */
+    double ret_prob;
+    Animal(){
+        /**
+         * All agents start with 0 payoff
+         */
+        payoff = 0;
+        win = false; // 0
+        seriously_injured = false;
+        /**
+         * Dangerous attacks 'D' have high probability of serious damage
+         */
+        prob_injury = 0.4; // Probability of serious damage from an attack.
+        /**
+         * Retaliator will retaliate with high probability of a dangerous attack  
+         */ 
+        ret_prob = 0.9; // Retaliation probability. 
 
-    Animal(){};
-    char mouse(const char &opp_move);
-    char hawk(const char &opp_move);
-    char bully(const char &opp_move, const int &move);
+    };
+    /**
+     * Helper function to generate uniform random variables. 
+     */
+    double get_rv();
 };
 
-char Animal::mouse(const char &opp_move){
-    if(opp_move == 'D'){
-        next_strategy = 'R';
-    }else{
-        next_strategy = 'C';
-    }
-    return next_strategy;
-}
-char Animal::hawk(const char &opp_move){
-    if(!seriously_injured){
-        next_strategy='D';
-    }else{
-        next_strategy='R';
-    }
-    return next_strategy;
-}
-char Animal::bully(const char &opp_move, const int &move){
-    if(move==0){
-        next_strategy='D';
-    }
-    if(opp_move=='C'){
-        next_strategy='D';
-    }else if(opp_move=='D'){
-        
-    }
-    return next_strategy;
+double Animal::get_rv(){
+    {
+    std::random_device rv;
+    std::mt19937_64 gen(rv());
+    std::uniform_real_distribution<double> dist(0.0, 1.0);
+    return dist(gen);
+};
 
 }
+// Mouse class 
+
+class Mouse: public Animal{
+    public: 
+    char& rule(const char & opp_move);
+};
+char& Mouse::rule(const char& opp_move){
+    if(opp_move == 'D'){
+        next_move= 'R';
+    }else if (opp_move == 'C'){
+        next_move = 'C';
+        prev_move ='C';
+    }else if(opp_move =='R'){
+        next_move = 'C';
+        prev_move= 'C';
+        win=true; // 1 
+    }else{
+        std::cerr<<"That is an illegal move"<<std::endl;
+    };
+    return next_move;
+};
+
+// Hawk Class 
+
+class Hawk: public Animal{
+    public:
+    char& rule(const char& opp_move);
+};
+
+char& Hawk::rule(const char& opp_move){
+    if(opp_move == 'D'){
+        double u = get_rv();
+        if(u < prob_injury){
+            next_move ='R';
+            prev_move ='R';
+            win=false;
+            seriously_injured=true;
+            payoff -= 100;
+        }else{
+            next_move = 'D';
+            prev_move ='D';
+
+            payoff -= 2;
+        }
+    }
+    return next_move;
+};
+
+// Bully class 
+
+class Bully: public Animal{
+    public:
+    char& rule(const char& opp_curr_move, const char& opp_prev_move); 
+};
+
+char& Bully::rule(const char& opp_curr_move, const char& opp_prev_move){
+    if(opp_curr_move =='C'){
+        next_move = 'D';
+        prev_move ='D';
+
+    }else if(opp_curr_move =='D' && opp_prev_move=='D'){
+        double u = get_rv();
+        if(u < prob_injury){
+            next_move ='R';
+            prev_move ='R';
+            win=false;
+            seriously_injured=true;
+            payoff -= 100;
+
+        }else{
+            next_move = 'R';
+            prev_move ='R';
+            win=false;
+            payoff -= 2;
+
+        }
+    }else if(opp_curr_move =='D'){
+        double u = get_rv();
+        if(u < prob_injury){
+            next_move ='R';
+            prev_move ='R';
+            win=false;
+            seriously_injured=true;
+            payoff -= 100;
+        }else{
+            next_move = 'C';
+            prev_move ='C';
+            payoff -= 2;
+        }
+    }
+    return next_move;
+}
+
+// Retaliator class 
+
+class Retliator: public Animal{
+    public:
+    char& rule(const char& opp_move);
+};
+
+// Retliator update rule
+
+char& Retliator::rule(const char& opp_move){
+    if(opp_move =='C'){
+        next_move='C';   
+        prev_move = 'C';  
+    }else if(opp_move == 'D'){
+        double u = get_rv();
+        if(u < prob_injury){
+            next_move ='R';
+            prev_move ='R';
+            win=false;
+            seriously_injured=true;
+            payoff -= 100;
+        }else{
+            double u = get_rv();
+            if(u < ret_prob){
+                next_move ='D';
+                prev_move = 'D';
+            }else{
+                next_move ='C';
+                prev_move = 'C';
+            };
+                payoff -= 2;
+        };
+    }else if(opp_move =='R'){
+        win=true;
+    }else{
+        std::cerr<<"The opponents move is illegal"<<std::endl;
+    };
+    return next_move;
+};
